ad3f8832d754ccaf5624a7ebf035dfa6b9fec622
=ptalluru <=talluruprasanthkumar04071995@gmail.com>
2019-01-22 18:35:06 +0530

    Adds the sample eureka demo

---
+++ Eureka-Demo/hello-world/src/main/java/com/cg/app/helloworld/resource/HelloResource.java
@@ -0,0 +1,18 @@
+package com.cg.app.helloworld.resource;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/helloresources")
+public class HelloResource {
+	
+	@GetMapping
+	public ResponseEntity<String> hello(){
+		return new ResponseEntity<String>("HELLO WORLD",HttpStatus.OK);
+	}
+
+}
---
+++ Eureka-Demo/website/src/main/java/com/cg/app/website/controller/WebsiteController.java
@@ -0,0 +1,29 @@
+package com.cg.app.website.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.client.RestTemplate;
+
+@Controller
+public class WebsiteController {
+
+	@Autowired
+	RestTemplate restTemplate;
+
+	@RequestMapping("/")
+	public String index() {
+		return "index";
+	}
+
+	@RequestMapping("/hello")
+	public String hello(Model model) {
+		ResponseEntity<String> entity = restTemplate.getForEntity("http://ajay/helloresources", String.class);
+		String hello = entity.getBody();
+		model.addAttribute("hello", hello);
+		return "hello";
+	}
+
+}
